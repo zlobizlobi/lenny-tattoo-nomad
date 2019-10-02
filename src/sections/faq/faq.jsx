07886d6197aee656f704c35bea0fb0610b43af45
David <bureauzlobi@gmail.com>
2019-10-02 21:58:41 +0200

    Change question fontweight on faq component

--- src/sections/faq/faq.jsx
+++ src/sections/faq/faq.jsx
@@ -12,7 +12,7 @@ export const Faq = () => (
           <FaqComponent
             content={{
               question:
-                "DOES IT HURT...",
+                "does it hurt...",
               answer:
                 "simple answer is, yes it does. but it’s also very different from person to person - some have a high tolerance for pain, some don’t. no matter what - people always expect worse than it actually is."
             }}
@@ -20,7 +20,7 @@ export const Faq = () => (
           <FaqComponent
             content={{
               question:
-                "HOW MUCH DOES IT COST...",
+                "how much does it cost...",
               answer:
                 "each project of a tattoo is very individual and requires personal attention - placement, size, colors, design etc. Therefore it is very hard to say beforehand how much a tattoo will cost. Let's discuss!"
             }}
@@ -28,7 +28,7 @@ export const Faq = () => (
           <FaqComponent
             content={{
               question:
-                "HOW LONG DOES A TATTOO TAKe...",
+                "how long does a tattoo take...",
               answer:
                 "tattooing is not a quick process, nor should it be rushed as you will be living with the results permanently. again, depends on the tattoo - contact me!"
             }}

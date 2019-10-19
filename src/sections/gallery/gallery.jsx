31e7c142e611ff9c76c73d5ceae1f760dec30d21
David Zlobinskyy <bureauzlobi@gmail.com>
2019-10-19 16:01:46 +0200

    Add react image lightbox to lenny's website

--- src/sections/gallery/gallery.jsx
+++ src/sections/gallery/gallery.jsx
@@ -4,15 +4,26 @@ import * as React from "react";
 import { Container, Overlay } from "./components";
 import { Image } from "../../components";
 import { useGetFirebaseImages } from "./hooks";
+import Lightbox from 'react-image-lightbox';
+import 'react-image-lightbox/style.css'; // This only needs to be imported once in your app
 
 export const Gallery = () => {
   const [selections, setSelected] = React.useState([]);
 
+  const [isOpen, setIsOpen] = React.useState(false)
+  const [photoIndex, setPhotoIndex] = React.useState(0)
+
   const handleOnClick = id => {
-    setSelected([id]);
+    setIsOpen(!isOpen);
   };
 
+  const handleOnMovePrevRequest = () => {
+    setPhotoIndex((photoIndex + firebaseImages.length - 1) % firebaseImages.length)
+  }
 
+  const handleOnMoveNextRequest = () => {
+    setPhotoIndex((photoIndex + 1) % firebaseImages.length)
+  }
 
   const firebaseImages = useGetFirebaseImages();
 
@@ -23,12 +34,21 @@ export const Gallery = () => {
           <Image
             intrinsicSize="100 x 100"
             alt="Picture of a Tattoo"
-            isViewed={selections.includes(index)}
-            onClick={() => handleOnClick(index)}
             key={imageUrl}
             src={imageUrl}
+            onClick={() => setIsOpen(true)}
           />
         ))}
+        {isOpen && (
+          <Lightbox
+            mainSrc={firebaseImages[photoIndex]}
+            nextSrc={firebaseImages[(photoIndex + 1) % firebaseImages.length]}
+            prevSrc={firebaseImages[(photoIndex + firebaseImages.length - 1) % firebaseImages.length]}
+            onCloseRequest={handleOnClick}
+            onMovePrevRequest={handleOnMovePrevRequest}
+            onMoveNextRequest={handleOnMoveNextRequest}
+          />
+        )}
       </Container>
     </Scroll.Element>
   );

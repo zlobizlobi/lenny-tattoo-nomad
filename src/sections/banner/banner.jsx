d0d178c83eaf23eb132c3113b4a504255d4a9244
David Zlobinskyy <bureauzlobi@gmail.com>
2019-10-23 14:33:21 +0200

    Clean up banner section

--- src/sections/banner/banner.jsx
+++ src/sections/banner/banner.jsx
@@ -2,50 +2,26 @@ import * as React from "react";
 import * as Scroll from "react-scroll";
 import "firebase/storage";
 import whitelogo from "../../../public/whitelogo.png";
-import { FaPhone } from 'react-icons/fa'
 import {
   Video,
   Logo,
   Container,
-  LogoPlaceHolder
+  LogoPlaceHolder,
+  Button,
+  PhoneIcon
 } from "./components";
 import { SocialMedia } from "../../components";
 import { useGetBannerVisualContent } from "./useGetBannerVisualContent";
-import styled from 'styled-components'
 
 
-const Button = styled.button`
-  transition: color 0.2s ease;
-  display: flex;
-  align-items: center;
-  border: 1px solid white;
-  padding: 11px 20px;
-  background-color: transparent;
-  color: white;
-  cursor: pointer;
-  font-weight: 700;
-  font-size: 16px;
-  transition: all 0.3s ease;
 
-  &:hover {
-    background-color: white;
-    color: hsl(0, 60%, 40%);
-  }
-`
-
-export const PhoneIcon = styled(FaPhone)`
-  margin-left: 10px;
-  font-size: 12px;
-  transition: color 0.3s ease;
-
-`
 
 export const Banner = () => {
   const { webm, mp4 } = useGetBannerVisualContent();
 
   return (
-    <Scroll.Element name="home-section">
-      <section>
+    <section>
+      <Scroll.Element name="home-section">
         <Container>
           {!(mp4 || webm) && <LogoPlaceHolder src="whitelogo.png" />}
           {(mp4 || webm) && (
@@ -61,7 +37,7 @@ export const Banner = () => {
             </React.Fragment>
           )}
         </Container>
-      </section>
-    </Scroll.Element >
+      </Scroll.Element >
+    </section>
   );
 };
--- src/sections/banner/components.js
+++ src/sections/banner/components.js
@@ -1,14 +1,13 @@
 import styled from "styled-components";
 import { breakpoint } from "../../styles/breakpoints";
+import { FaPhone } from 'react-icons/fa'
 
 export const Video = styled.video`
-  height: 100vh;
-  width: 100vw;
-  max-width: 100vw;
-  max-height: 100vh;
+  height: 100%;
+  width: 100%;
   object-fit: cover;
   position: absolute;
-  z-index: -1;
+  z-index: 0;
 `;
 
 export const Container = styled.div`
@@ -18,15 +17,13 @@ export const Container = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: space-evenly;
-  background-color: transparent;
 `;
 
 export const Logo = styled.img.attrs({ alt: "Logo of Lenny Tattoo Nomad" })`
   position: relative;
-  z-index: 100;
+  z-index: 1;
   width: 200px;
   opacity: 0.6;
-  margin-top: 60px;
   
   @media ${breakpoint.tablet} {
     width: 350px
@@ -41,5 +38,30 @@ export const LogoPlaceHolder = styled.img.attrs({
 })`
   opacity: 0.3;
   width: 200px;
-  height: 200px;
 `;
+
+
+export const Button = styled.button`
+  display: flex;
+  align-items: center;
+  position: relative;
+  z-index: 1;
+  border: 1px solid white;
+  padding: 11px 20px;
+  background-color: transparent;
+  color: white;
+  cursor: pointer;
+  font-weight: 700;
+  font-size: 16px;
+  transition: all 0.3s ease;
+
+  &:hover {
+    background-color: white;
+    color: hsl(0, 60%, 40%);
+  }
+`
+
+export const PhoneIcon = styled(FaPhone)`
+  margin-left: 10px;
+  font-size: 12px;
+`

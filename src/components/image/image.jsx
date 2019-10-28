5006dfc445243ca34c7543aeb2d5001427ca7ceb
David Zlobinskyy <bureauzlobi@gmail.com>
2019-10-28 08:07:36 +0100

    Add filter to gallery images

--- src/components/image/image.jsx
+++ src/components/image/image.jsx
@@ -3,28 +3,32 @@ import styled from "styled-components";
 import { breakpoint } from "../../styles/breakpoints";
 
 const ImageComponent = styled.img`
-  border: 2px solid hsl(0, 60%, 35%);
-  margin: 10px 0;
   width: 160px;
   height: 160px;
   cursor: pointer;
-
+  filter: grayscale(100%);
+  display: block;
+  transition: filter 0.2s ease;
   :hover {
     filter: grayscale(0%);
   }
+`;
+
+const ImageContainer = styled.div`
+  display: inline;
+  border: 2px solid hsl(0, 60%, 35%);
+  margin: 10px 0;
 
   @media ${breakpoint.mobileL} {
     margin: 5px;
   }
-`;
-
-export const Image = ({ src, onClick, alt }) => {
-  return (
+`
+export const Image = ({ src, onClick, alt }) =>
+  <ImageContainer>
     <ImageComponent
       src={src}
       onClick={onClick}
       alt={alt}
       loading="lazy"
     />
-  );
-};
+  </ImageContainer>
